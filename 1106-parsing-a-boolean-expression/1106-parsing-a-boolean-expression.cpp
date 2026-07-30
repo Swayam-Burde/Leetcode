31f56b802064a682c2437447c6976d332caebcfa
Swayam Burde <swayamburde2004@gmail.com>
2026-07-30 22:43:48 +0530

    Time: 0 ms (100%), Space: 9.7 MB (90.35%) - LeetHub

---
+++ 1106-parsing-a-boolean-expression/1106-parsing-a-boolean-expression.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    bool parseBoolExpr(string expression) {
+        vector<char> st;
+        
+        for (char c : expression) {
+            if (c == ',' || c == '(') {
+                continue;
+            }
+            if (c == 't' || c == 'f' || c == '!' || c == '&' || c == '|') {
+                st.push_back(c);
+            } else if (c == ')') {
+                bool hasTrue = false, hasFalse = false;
+                
+                while (st.back() == 't' || st.back() == 'f') {
+                    if (st.back() == 't') hasTrue = true;
+                    if (st.back() == 'f') hasFalse = true;
+                    st.pop_back();
+                }
+                
+                char op = st.back();
+                st.pop_back();
+                
+                if (op == '!') {
+                    st.push_back(hasTrue ? 'f' : 't');
+                } else if (op == '&') {
+                    st.push_back(hasFalse ? 'f' : 't');
+                } else if (op == '|') {
+                    st.push_back(hasTrue ? 't' : 'f');
+                }
+            }
+        }
+        
+        return st.back() == 't';
+    }
+};
