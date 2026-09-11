e802096cc6d14a2f2230792059bb39565eab43f4
Swayam Burde <swayamburde2004@gmail.com>
2026-09-11 20:17:48 +0530

    Time: 0 ms (100%), Space: 9 MB (51.39%) - LeetHub

--- 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.cpp
+++ 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.cpp
@@ -1,12 +1,11 @@
 class Solution {
 public:
     long long countCommas(long long n) {
-        if(n < 1000) return 0;
-        if(n >= 1000 && n < 1000000) return n - 1000 + 1;
-        if(n >= 1000000 && n < 1000000000) return (n -1000 + 1) + (n - 1000000 + 1);
-        if(n >= 1000000000 && n < 1000000000000) return (n - 1000 + 1) + (n - 1000000 + 1) + (n - 1000000000 + 1);
-        if(n >= 1000000000000 && n < 1000000000000000) return (n - 1000 + 1) + (n - 1000000 + 1) + (n - 1000000000 + 1) + (n - 1000000000000 + 1);
-        if(n == 1000000000000000) return (n - 1000 + 1) + (n - 1000000 + 1) + (n - 1000000000 + 1) + (n - 1000000000000 + 1) + 1;
-        return 0;
+        long long ans = 0;
+    for (long long i = 1000; i <= n; i *= 1000) {
+        ans += (n - i + 1);
+        if (i > LLONG_MAX / 1000) break;
+    }
+    return ans;
     }
 };
