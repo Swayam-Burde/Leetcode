2dfc5025c6de21c38a27f20d780ef6f374804005
Swayam Burde <swayamburde2004@gmail.com>
2026-09-14 20:58:55 +0530

    Time: 143 ms (9.87%), Space: 54.8 MB (13.31%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -1,35 +1,33 @@
 class Solution {
 public:
-    bool isAllStar(const string &p, int k) {
-        for (int j = 1; j <= k; j++) {
-            if (p[j - 1] != '*') {
+    bool f(string& s,int j){
+        for(int i = j; i < s.size();i++){
+            if(s[i] != '*'){
                 return false;
             }
         }
         return true;
-    } 
+    }
+    bool solve(int i, int j, string& s, string& p, vector<vector<int>>& dp) {
+        if (i == s.size()) {
+            return f(p, j);
+        }
+        if (j == p.size()) {
+            return false;
+        }
+        if (dp[i][j] != -1) return dp[i][j];
+        if (i < s.size() && s[i] == p[j]) {
+            return dp[i][j] = solve(i + 1, j + 1, s, p,dp);
+        } else if ( i < s.size() && p[j] == '?') {
+            return dp[i][j] = solve(i + 1, j + 1, s, p, dp);
+        } else if (i < s.size() && p[j] == '*') {
+            return dp[i][j] = solve(i, j + 1, s, p, dp)  || solve(i+1,j,s,p, dp);
+        } 
+        return dp[i][j] = false;
+    }
 
     bool isMatch(string s, string p) {
-        int n = s.size();
-        int m = p.size();
-        vector<vector<bool>> dp(n + 1, vector<bool>(m + 1, false));
-        dp[0][0] = true;
-        for (int j = 1; j <= m; j++) {
-            dp[0][j] = isAllStar(p, j);
-        }
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= m; j++) {
-                if (s[i - 1] == p[j - 1] || p[j - 1] == '?') {
-                    dp[i][j] = dp[i - 1][j - 1];
-                } 
-                else if (p[j - 1] == '*') {
-                    dp[i][j] = dp[i - 1][j] || dp[i][j - 1];
-                } 
-                else {
-                    dp[i][j] = false;
-                }
-            }
-        }
-        return dp[n][m];
+        vector<vector<int>> dp(s.size(), vector<int>(p.size(), -1));
+        return solve(0, 0, s, p,dp);
     }
 };
