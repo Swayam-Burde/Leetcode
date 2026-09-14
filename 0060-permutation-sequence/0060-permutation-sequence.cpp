6a1d977cec4e2ed04bc829dc01add37e97713397
Swayam Burde <swayamburde2004@gmail.com>
2026-09-14 18:08:25 +0530

    Time: 51 ms (16.08%), Space: 8.5 MB (74.49%) - LeetHub

---
+++ 0060-permutation-sequence/0060-permutation-sequence.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string getPermutation(int n, int k) {
+        string num = "";
+        string target = "";
+        vector<string> str;
+        for (int i = 1; i <= n; i++) {
+            num = num + to_string(i);
+        }
+
+        for (int i = 0; i < k - 1; i++) {
+            next_permutation(num.begin(), num.end());
+        }
+        return num;
+    }
+};
