9b13f48c64793edd44a1a55eaf5321c67975e21b
Swayam Burde <swayamburde2004@gmail.com>
2026-09-16 20:22:50 +0530

    Time: 11 ms (20.36%), Space: 15.2 MB (15.32%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.cpp
+++ 0047-permutations-ii/0047-permutations-ii.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
-    void generate(int i, vector<int>& nums, set<vector<int>>& ans){
+    void generate(int i, vector<int>& nums, vector<vector<int>>& ans){
         if(i == nums.size()){
-            ans.insert(nums);
+            ans.push_back(nums);
             return;
         }
+        unordered_set<int> st;
         for(int j = i; j < nums.size(); j++){
-            if(nums[i] == nums[j]){
-                generate(i+1,nums,ans);
-            }else{
-                swap(nums[i],nums[j]);
-                generate(i+1,nums,ans);
-                swap(nums[i],nums[j]);
+            if(st.find(nums[j]) != st.end()){
+                continue;
             }
+            st.insert(nums[j]);
+            swap(nums[i],nums[j]);
+            generate(i+1,nums,ans);
+            swap(nums[i],nums[j]);
         }
     }
     vector<vector<int>> permuteUnique(vector<int>& nums) {
-        set<vector<int>> ans;
+        vector<vector<int>> ans;
         generate(0,nums,ans);
-        vector<vector<int>> res(ans.begin(),ans.end());
-        return res;
+        return ans;
     }
 };
