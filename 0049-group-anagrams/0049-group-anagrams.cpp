1ad6f63abd3f5ec43269de47243f7a7834242dac
Swayam Burde <swayamburde2004@gmail.com>
2026-09-17 16:56:21 +0530

    Time: 12 ms (82.93%), Space: 25.1 MB (73.55%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,42 +1,17 @@
 class Solution {
 public:
-    bool isAnagram(string& s, string& t){
-        if(s.size() != t.size()){
-            return false;
-        }
-        int freq[26] = {0};
-        for(int i = 0; i < s.size(); i++){
-            freq[s[i] - 'a']++;
-        }
-        for(int i = 0; i < t.size(); i++){
-            freq[t[i] - 'a']--;
-        }
-        for(int i = 0; i < 26; i++){
-            if(freq[i] != 0){
-                return false;
-            }
-        }
-        return true;
-    }
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        vector<vector<string>> ans;
-        vector<string> temp;
-        int n = strs.size();
-        vector<bool> valid(n, true);
-        for(int i = 0; i < n; i++){
-            if(valid[i] == false){
-                continue;
-            }
-            temp.push_back(strs[i]);
-            for(int j = i + 1;j < n; j++){
-                if(isAnagram(strs[i],strs[j])){
-                    temp.push_back(strs[j]);
-                    valid[j] = false;
-                }
-            }
-            ans.push_back(temp);
-            temp.clear();
+        unordered_map<string, vector<string>> mp;
+        
+        for (string& s : strs) {
+            string key = s;
+            sort(key.begin(), key.end());
+            mp[key].push_back(s); 
         }
+        vector<vector<string>> ans;
+        for (auto& pair : mp) {
+            ans.push_back(pair.second);
+        } 
         return ans;
     }
 };
