3cd932345f458e1f420430bc3f39c632d25967b8
Swayam Burde <swayamburde2004@gmail.com>
2026-09-21 16:23:14 +0530

    Time: 0 ms (100%), Space: 11.9 MB (94.32%) - LeetHub

--- 0682-baseball-game/0682-baseball-game.cpp
+++ 0682-baseball-game/0682-baseball-game.cpp
@@ -3,20 +3,25 @@ public:
     int calPoints(vector<string>& operations) {
         int n = operations.size();
         vector<int> ans;
+        int res = 0;
         for(int i = 0; i < n ; i++){
             if(operations[i] == "C"){
+                res -= ans.back();
                 ans.pop_back();
             }
             else if(operations[i] == "D"){
+                res += (2*ans.back());
                 ans.push_back(2*ans.back());
             }
             else if(operations[i] == "+"){
+                res += ans[ans.size()-1] + ans[ans.size()-2];
                 ans.push_back(ans[ans.size()-1] + ans[ans.size()-2]);
             }
             else{
                 ans.push_back(stoi(operations[i]));
+                res += ans.back();
             }
         }
-        return accumulate(ans.begin(),ans.end(), 0);
+        return res;
     }
 };
